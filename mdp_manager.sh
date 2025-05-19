4f0cd479cbbaaa18418d1e2c800e0f024359269b
lucas_broda <lulubrod@gmail.com>
2025-05-19 11:22:39 +0100

    feat(mdp_manager.sh): started to add some options on the menu

--- mdp_manager.sh
+++ mdp_manager.sh
@@ -19,25 +19,48 @@ echo "Le fichier existe"
 read -s -p "Entrez le mot de passe maître : " entered
 echo
 
-# Hachage et comparaison du mot de passe mâitre
+# Hachage et comparaison
 entered_hash=$(echo -n "$entered" | sha256sum | awk '{print $1}')
 stored_hash=$(cat ~/.passmaster)
 
 if [ "$entered_hash" != "$stored_hash" ]; then
-    echo "Mot de passe incorrect. Accès refusé."
+    echo "[🚫] Mot de passe incorrect. Accès refusé."
     exit 1
 fi
 
-echo "✅ Accès autorisé."
+echo "[✅] Accès autorisé."
 
 
 while true; do 
         echo "===Gestionnaire Password===="
         echo "1. Ajouter un mot de passe"
         echo "2. Consulter mode de passe"
-        echo "3. Delete mot de passe"
-        echo "4. Quitter"
+	echo "3. Modification d'un/des mot(s) de passe"
+        echo "4. Delete mot de passe"
+        echo "5. Quitter"
         read -p "choix : " choice
-        
-done
 
+	case "$choice" in
+	1)
+		echo "=== Ajouter un nouveau mot de passe ==="
+            	read -p "Adresse mail / nom utilisateur : " id
+           	read -s -p "Mot de passe : " pwd
+            	echo
+            	gpg -d "$PASSWORD_FILE" > temp.txt 2>/dev/null
+            	echo "$id -> $pwd" >> temp.txt
+            	gpg --symmetric --cipher-algo AES256 -o "$PASSWORD_FILE" temp.txt
+            	rm temp.txt
+            	echo "[✅] Mot de passe ajouté."
+            	;;
+
+	5)
+		 read -p "[❓] Êtes-vous sûr de vouloir quitter ? (o/n) : " confirm
+    			if [[ "$confirm" =~ ^[oO]$ ]]; then
+       				echo "[👋] Au revoir !"
+        			exit 0
+    			else
+        			echo "[🔄] Retour au menu."
+    			fi
+   		 ;;
+	esac
+done
