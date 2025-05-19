b25ee9c7026a60dd913e3ea8fff980bb3218d3cf
lucas <lucas.olier2003@icloud.com>
2025-05-19 08:13:29 +0000

    Init Project

---
+++ mdp_manager.sh
@@ -0,0 +1,10 @@
+while true; do 
+        echo "===Gestionnaire Password===="
+        echo "1. Ajouter un mot de passe"
+        echo "2. Consulter mode de passe"
+        echo "3. Delete mot de passe"
+        echo "4. Quitter"
+        read -p "choix : " choice
+        
+done
+
